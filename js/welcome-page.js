97c3fcd8679cf1409a19c1cc3214f881521ec7e7
gabs1bb3 <sanjeev1bb3@protonmail.com>
2017-11-30 16:44:00 +0545

    Fixed Prevent Login Bypass. Previously site/welcome-page.html could be accessed by typing the url in browser which a check should have prevented, now it workd properly. Fucking bug.

--- js/welcome-page.js
+++ js/welcome-page.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
     var auth = Cookies.getJSON('auth');
     console.log(auth);
-    if(auth.auth){
+    if(auth){
         $("h4").text("Welcome "+auth.user+"!");
     }else{
+        console.log("Bypassed Login\nRedirecting to login page...");
         location.replace("../index.html");//In case someone gets access to this page without auth
     };
 });
