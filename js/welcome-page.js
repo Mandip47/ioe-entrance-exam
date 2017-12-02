b3b8767d9eb7159c13d06910b928dca7207b5cdf
gabs1bb3 <sanjeev1bb3@protonmail.com>
2017-12-02 22:43:53 +0545

    Fixed The Welcome <user>. Now the will redirect before documents loads in case of login bypass. And Welcome <User> will be updated after page loads.

--- js/welcome-page.js
+++ js/welcome-page.js
@@ -1,10 +1,18 @@
     var auth = Cookies.getJSON('auth');
     console.log(auth);
-    if(auth){
-        $("h4").text("Welcome "+auth.user+"!");
-    }else{
+    if(!auth){
         console.log("Bypassed Login\nRedirecting to login page...");
         location.replace("../index.html");//In case someone gets access to this page without auth
     };
+
+    function noob() {
+        if (auth.user == "Ishani"){
+            alert("What the !@#$ are you doing here.\nGo study your !@#$ing Biology, NOOB");
+        }
+    }
+
+    $(document).ready(function(){
+        $("h4").text("Welcome "+auth.user+"!");
+    });
 
 //Take examination still left.
