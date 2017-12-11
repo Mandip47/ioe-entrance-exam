56fc5830dd980a7d97fbbf47caf9bc0f82a2b2f1
Sanjeev Kc <sanjeev1BB3@protonmail.com>
2017-12-11 23:37:23 +0545

    Update welcome-page.js

--- js/welcome-page.js
+++ js/welcome-page.js
@@ -5,12 +5,6 @@
         location.replace("../index.html");//In case someone gets access to this page without auth
     };
 
-    function noob() {
-        if (auth.user == "Ishani"){
-            alert("What the !@#$ are you doing here.\nGo study your !@#$ing Biology, NOOB");
-        }
-    }
-
     $(document).ready(function(){
         $("h4").text("Welcome "+auth.user+"!");
     });
